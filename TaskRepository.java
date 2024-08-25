16ba14064d6b04b767a09618596c79a2561a9134
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-08-25 21:11:35 +0530

    Create TaskRepository.java

---
+++ TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.taskmanagement.repositories;
+
+import com.example.taskmanagement.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId);
+}
