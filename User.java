c16622c5aee267dd0b88d72f308b1b33d6d964b7
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-08-25 21:08:28 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,22 @@
+package com.example.taskmanagement.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Task> tasks;
+
+    // Getters and Setters
+}
